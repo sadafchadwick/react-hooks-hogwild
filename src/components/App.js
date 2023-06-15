2582be55eb57d1a91bfe5dd738033c9fb540dbaa
Sadaf Chadwick <sadafchadwick@gmail.com>
2023-06-15 09:32:10 -0500

    trying to figure it out

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Nav from "./Nav";
-
+import Card from "./Card.js"
 import hogs from "../porkers_data";
 
 function App() {
+	const hogCards = hogs.map((hog) => {
+		// console.log(hog)
+		return <Card 
+					name = {hog.name}
+					image = {hog.image}
+				/>
+	})
 	return (
 		<div className="App">
 			<Nav />
+			{hogCards}
 		</div>
 	);
 }
